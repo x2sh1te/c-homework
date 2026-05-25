276e6bb4115252975e016a9ac34c126a359543c6
x2sh1te <nikitabalvurov1506@gmail.com>
2026-05-25 18:19:53 +0800

    Update main_bfs.cpp

--- graph/main_bfs.cpp
+++ graph/main_bfs.cpp
@@ -1,17 +1,21 @@
 #include "bfs.h"
 #include <set>
+#include <iostream>
 
 int main() {
-    Graph g("TestGraph.txt");
+    Graph g("1000.csv"); 
+
     std::set<Node*> unexplored(g.begin(), g.end());
     int component_no = 1;
     while (!unexplored.empty()) {
         Node* start = *(unexplored.begin());
         std::set<Node*> comp;
-        BFS(g).component(start, comp);
+        bfs_component(start, comp);
         write_component_to_file(comp, component_no);
+        std::cout << "Component " << component_no << " size: " << comp.size() << std::endl;
         for (Node* n : comp) unexplored.erase(n);
         ++component_no;
     }
+    std::cout << "Total components: " << component_no - 1 << std::endl;
     return 0;
 }
