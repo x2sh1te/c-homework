9055215744253045b857b18fd191e383853382ad
x2sh1te <nikitabalvurov1506@gmail.com>
2026-05-25 18:19:31 +0800

    Update bfs.h

--- graph/bfs.h
+++ graph/bfs.h
@@ -1,45 +1,48 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#include <string>
+#include <iostream>
 #include <set>
 #include <queue>
 #include <fstream>
 #include <map>
+#include <string>
+
+class Node;
+
+class Graph {
+    std::set<Node*> nodes;
+    std::map<std::string, Node*> name2node;
+
+public:
+    Graph(const char* file_name);
+    ~Graph();
+    Node* findNode(const std::string& name);
+    void addNode(const std::string& name);
+    void addEdge(const std::string& from, const std::string& to);
+    auto begin() const { return nodes.begin(); }
+    auto end() const { return nodes.end(); }
+};
 
 class Node {
     std::string name;
     std::set<Node*> neighbours;
-    void addNeighbour(Node* neighbour);
-    void removeNeighbour(Node* neighbour);
+
+    void addNeighbour(Node* n);
+    void removeNeighbour(Node* n);
+
 public:
-    Node(const std::string& aname);
-    const std::string& getName() const;
-    auto nb_begin() const;
-    auto nb_end() const;
+    Node(const std::string& n) : name(n) {}
+    const std::string& getName() const { return name; }
+    auto nb_begin() const { return neighbours.begin(); }
+    auto nb_end() const { return neighbours.end(); }
+
     friend class Graph;
 };
 
-class Graph {
-    std::set<Node*> nodes;
-    std::map<std::string, Node*> name2node; // для поиска по имени
-public:
-    Graph(const char* file_name);
-    ~Graph();
-    void addNode(const std::string& name);
-    void addEdge(const std::string& from, const std::string& to);
-    auto begin() const;
-    auto end() const;
-    Node* findNode(const std::string& name) const;
-};
+// BFS для выделения компоненты связности
+void bfs_component(Node* start, std::set<Node*>& comp);
 
-class BFS {
-    const Graph& graph;
-public:
-    BFS(const Graph& agraph);
-    void component(Node* start, std::set<Node*>& out_nodes);
-};
-
-void write_component_to_file(const std::set<Node*>& comp, int idx);
+void write_component_to_file(const std::set<Node*>& comp, int id);
 
 #endif // GRAPH_H
